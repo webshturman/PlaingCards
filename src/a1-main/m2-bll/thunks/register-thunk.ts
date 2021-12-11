50ea8cfcf6fc174a68db4d89f1fcf820315234cb
KirillProgMaster <kirill.zarya@inbox.ru>
2021-12-11 23:04:55 +0300

    create register form refactoring

--- src/a1-main/m2-bll/thunks/register-thunk.ts
+++ src/a1-main/m2-bll/thunks/register-thunk.ts
@@ -7,11 +7,9 @@ import { credentialsType } from 'a1-main/m3-dal/types/registerType';
 export const toSignUp =
   (credentials: credentialsType): AppThunk =>
   async dispatch => {
-    // dispatch(ChangeLoadingStatusAC('loading'));
     try {
-      const res = await registerAPI.signUp(credentials);
+      await registerAPI.signUp(credentials);
       dispatch(setRegisterStatus(true));
-      console.log(res);
     } catch (error: any) {
       console.log(error);
     }
--- src/a1-main/m3-dal/instance.ts
+++ src/a1-main/m3-dal/instance.ts
@@ -4,7 +4,4 @@ export const instance = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
   baseURL: 'https://neko-back.herokuapp.com/2.0/',
   withCredentials: true,
-  // headers: {
-  //     'API-KEY': 'b8b0c036-4edd-4913-8c7f-79ccaeace603'
-  // },
 });
