9d5ce62b4c93bf35a91f6835571ace062407b57c
Anjeli-art <shinyanj@gmail.com>
2021-12-12 16:57:36 +0300

    password recovery and new password components

--- src/a2-features/NewPassword.tsx
+++ src/a2-features/NewPassword.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Button } from '../a1-main/m1-ui/components/common/CustomButton/Button';
+import { Input } from '../a1-main/m1-ui/components/common/CustomInput/Input';
+import { CONFIRM_PASSWORD, EMPTY_STRING, PASSWORD } from '../constants/common';
 
 import { ReturnComponentType } from 'types/ReturnComponentType';
 
-export const NewPassword = (): ReturnComponentType => <div>New Password</div>;
+export const NewPassword = (): ReturnComponentType => {
+  const [password, setPassword] = useState<string>(EMPTY_STRING);
+  const [passwordConfirm, setPasswordConfirm] = useState<string>(EMPTY_STRING);
+  const passLenght = 7;
+  const handleSubmit = (): void => {
+    if (password !== passwordConfirm || password.length < passLenght) {
+      console.log('The passwords do not match');
+      return;
+    }
+    console.log(password);
+    console.log(passwordConfirm);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          title={PASSWORD}
+          onChangeText={setPassword}
+          value={password}
+          type="password"
+        />
+        <Input
+          title={CONFIRM_PASSWORD}
+          onChangeText={setPasswordConfirm}
+          value={passwordConfirm}
+          type="password"
+        />
+        <div>
+          <Button>Register</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/a2-features/f3/RecoveryPassword.tsx
+++ src/a2-features/f3/RecoveryPassword.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Button } from '../../a1-main/m1-ui/components/common/CustomButton/Button';
+import { Input } from '../../a1-main/m1-ui/components/common/CustomInput/Input';
+import { EMPTY_STRING, TITLE_EMAIL } from '../../constants/common';
 
 import { ReturnComponentType } from 'types/ReturnComponentType';
 
-export const RecoveryPassword = (): ReturnComponentType => <div>Recovery Password</div>;
+export const RecoveryPassword = (): ReturnComponentType => {
+  const [email, setEmail] = useState<string>(EMPTY_STRING);
+  const handleSubmit = (): void => {
+    console.log(email);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input title={TITLE_EMAIL} onChangeText={setEmail} value={email} type="text" />
+        <div>
+          <Button>Register</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
